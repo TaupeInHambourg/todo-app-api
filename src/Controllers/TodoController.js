635177028d8750aaf7bdf4c60a89853d0b4bb88f
TaupeInHambourg <129371488+TaupeInHambourg@users.noreply.github.com>
2024-03-13 23:30:07 +0100

    jour 2

--- src/Controllers/TodoController.js
+++ src/Controllers/TodoController.js
@@ -2,10 +2,45 @@ const Todo = require('../models/Todo')
 
 async function getTodos () {
   const todos = await Todo.find()
-  console.log(todos)
   return todos
 }
 
+async function createTodo (todo) {
+  if (todo) {
+    const _todo = new Todo({
+      title: todo.title,
+      description: todo.description,
+      status: todo.status,
+      important: todo.important
+    })
+    _todo.save()
+  } else {
+    throw new Error('Todo is missing')
+  }
+}
+
+async function deleteTodoById (id) {
+  if (id) {
+    await Todo.deleteOne({ _id: id })
+  } else {
+    throw new Error('missing ID')
+  }
+}
+
+async function updateTodo (todo) {
+  if (todo) {
+    const { _id } = todo
+    const _todo = todo
+    delete _todo._id
+    await Todo.findByIdAndUpdate(_id, _todo)
+  } else {
+    throw new Error('missing todo')
+  }
+}
+
 module.exports = {
-  getTodos
+  getTodos,
+  createTodo,
+  deleteTodoById,
+  updateTodo
 }
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
-const { getTodos } = require('../Controllers/TodoController')
-const Todo = require('../models/Todo')
+const { getTodos, deleteTodoById, updateTodo, createTodo } = require('../Controllers/TodoController')
 
 router.route('/')
   .get(async (req, res) => {
@@ -14,15 +13,30 @@ router.route('/')
   })
 
   .post(async (req, res) => {
-    console.log(req.body)
     try {
-      const _todo = new Todo({
-        title: req.body.title,
-        description: req.body.description,
-        status: req.body.status,
-        important: req.body.important
-      })
-      _todo.save()
+      await createTodo(req.body)
+      const todos = await getTodos()
+      return res.json(todos)
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  })
+
+  .delete(async (req, res) => {
+    try {
+      await deleteTodoById(req.body.id)
+      const todos = await getTodos()
+      return res.json(todos)
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  })
+
+  .put(async (req, res) => {
+    try {
+      await updateTodo(req.body)
+      const todos = await getTodos()
+      return res.json(todos)
     } catch (error) {
       return res.status(500).send(error)
     }
