4967e176d787d73e4c99bfc4ae618a3fd0566488
TaupeInHambourg <129371488+TaupeInHambourg@users.noreply.github.com>
2024-03-15 21:49:09 +0100

    modal signup

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const loginUser = async (credentials, callback) => {
+  console.log('T_ES_LA?')
   let _error
 
   // Je vérifie la présence des paramètres
@@ -50,6 +51,55 @@ const loginUser = async (credentials, callback) => {
   })
 }
 
+const signupUser = async (credentials, callback) => {
+  let _error
+
+  if (!credentials.email || !credentials.password) {
+    _error = 'Invalid credentials'
+  }
+
+  const user = await User.findOne({ email: credentials.email })
+  if (!user) {
+    console.log('AUTH_CONTROLLER')
+    // User.createUser(
+    //   {
+    //     user: credentials.email,
+    //     pwd: credentials.password
+    //   }
+    // )
+
+    const _user = new User({
+      user: credentials.email,
+      pwd: credentials.password
+    })
+    await _user.save()
+
+    // const payload = {
+    //   id: user.id
+    // }
+
+    // jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '7d' }, (error, token) => {
+    //   if (error) {
+    //     _error = 'Invalid credentials'
+    //   }
+
+    //   // On supprime le mot de passe de l'utilisateur récupéré en base
+    //   const _user = user.toObject()
+    //   delete _user.password
+
+    //   // On retourne l'utilisateur et le token
+    //   return callback(_error, {
+    //     _user,
+    //     token
+    //   })
+    // })
+  } else {
+    _error = 'Invalid credentials'
+    return callback(_error, null)
+  }
+}
+
 module.exports = {
-  loginUser
+  loginUser,
+  signupUser
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,4 @@
-const { loginUser } = require('../Controllers/AuthController')
+const { loginUser, signupUser } = require('../Controllers/AuthController')
 
 const router = require('express').Router()
 
@@ -6,7 +6,24 @@ router.route('/login')
   .post(async (req, res) => {
     try {
       const credentials = req.body
-      loginUser(credentials, (error, result) => {
+      await loginUser(credentials, (error, result) => {
+        if (error) {
+          return res.status(403).send(error)
+        }
+        return res.send(result)
+      })
+    } catch (error) {
+      console.error(error)
+      return res.status(403).send(error)
+    }
+  })
+
+router.route('/register')
+  .post(async (req, res) => {
+    try {
+      const credentials = req.body
+      await signupUser(credentials, (error, result) => {
+        console.log('AUTH_CREDENTIALS')
         if (error) {
           return res.status(403).send(error)
         }
